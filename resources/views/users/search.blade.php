76618cc50b8a2edb4eb91bd8dc27e7a5f2dd4940
yuya1453 <yuya19991@outlook.jp>
2023-06-19 11:33:27 +0900

    foreach location

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -8,33 +8,32 @@
         <p class="fw-bold">Search result for <span class="fw-bold"></span></p>
 
         <table class="table table-hover align-middle borde">
-            <thead class="table">
-                <tr>
-                    <th>NAME</th>
-                    <th>DEPARTMENT</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody class="table table-hover align-middle border">
-                {{-- Foreach --}}
-                @forelse ($users as $user)
+            @forelse ($users as $user)
+                <thead class="table">
                     <tr>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->department}}</td>
-                        <td>
-                            @if (Auth::user()->id === $user->id)
-                                <a href="{{route('user.edit')}}" class="btn btn-secondary btn-sm"><i class="fa-solid fa-pen text-secondary"></i></a>
-                                <a href="{{route('user.destroy',$user->id)}}" class="btn btn-danger btn-sm" data-bs-toggle="nodal" data-bs-target="#" ><i class="fa-solid fa-trash-can "></i></a>
-                            @else
-                                <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#detail-user-{{$user->id}}">Details</button>
-                            @endif
-                        </td>
-                        @include('users.modal.detail')
+                        <th>NAME</th>
+                        <th>DEPARTMENT</th>
+                        <th>Actions</th>
                     </tr>
-                @empty
-                    <p class="lead text-muted text-center">No users found.</p>
-                @endforelse
-            </tbody>
+                </thead>
+                <tbody class="table table-hover align-middle border">
+                        <tr>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->department}}</td>
+                            <td>
+                                @if (Auth::user()->id === $user->id)
+                                    <a href="{{route('user.edit')}}" class="btn btn-secondary btn-sm"><i class="fa-solid fa-pen text-secondary"></i></a>
+                                    <a href="{{route('user.destroy',$user->id)}}" class="btn btn-danger btn-sm" data-bs-toggle="nodal" data-bs-target="#" ><i class="fa-solid fa-trash-can "></i></a>
+                                @else
+                                    <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#detail-user-{{$user->id}}">Details</button>
+                                @endif
+                            </td>
+                            @include('users.modal.detail')
+                        </tr>
+                </tbody>
+            @empty
+                <p class="lead text-muted text-center">No users found.</p>
+            @endforelse
         </table>
 
     </div>
